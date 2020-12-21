aef7c98259f6f7c1a22d34ac3953bee8d4f619b0
yaswanth <ynyashetty@gmail.com>
2020-12-21 20:29:11 +0530

    Add JavaScript to Code

---
+++ script.js
@@ -0,0 +1,8 @@
+
+//wait for window to load //
+document.add Event Listener("DoM content Loaded; function(event)")
+//Get reference to header object
+let my header =document.getElement by Id("header-id")
+//wait 3 seconds, then display the header
+set Timeout(c)=>{
+myheader.classList.remove(""hidden)
